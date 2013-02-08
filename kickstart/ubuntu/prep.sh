958afedaf93bb202bbb02268a172a0b48900dc08
root <root@DLPBMM1.cos.ucf.edu>
2013-02-08 11:11:29 -0500

    2013 Feb  8 [Fri] 11:11:29 AM :: root@DLPBMM1.cos.ucf.edu

--- kickstart/ubuntu/prep.sh
+++ kickstart/ubuntu/prep.sh
@@ -2,15 +2,12 @@
 echo Setting up initial softlinks
 ln -s ../../../builder/builder.sh .
 ln -s ../DEB .
-
 echo Copying kickstart templates
-cp          ../local.cfg.template    local.cfg
-cp       ../defaults.cfg.template defaults.cfg
-cp       ../packages.cfg.template packages.cfg
-
+cp             ../local.cfg.template    local.cfg
+cp          ../defaults.cfg.template defaults.cfg
+cp          ../packages.cfg.template packages.cfg
 echo Copying preseed templates
-cp ../preseed/preseed.local.template  local.preseed
-
+cp ../preseed/preseed.local.template    local.preseed
 echo Copying CGI templates
-cp              ../ks.sh.template       ks.sh
-cp ../preseed/preseed.sh.template  preseed.sh
+cp                 ../ks.sh.template       ks.sh
+cp    ../preseed/preseed.sh.template  preseed.sh
