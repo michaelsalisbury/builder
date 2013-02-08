976f670921bd797dffed52bde5845ae7d2203512
root <root@DLPBMM1.cos.ucf.edu>
2013-02-08 10:49:08 -0500

    2013 Feb  8 [Fri] 10:49:08 AM :: root@DLPBMM1.cos.ucf.edu

--- kickstart/ubuntu/s.apt-cache/preseed.sh
+++ kickstart/ubuntu/s.apt-cache/preseed.sh
@@ -8,13 +8,6 @@ echo "Content-Type: text/plain"
 echo # DO NOT REMOVE THIS BLANK LINE
 #############################################################################
 include local.preseed
-echo
 include ../preseed/preseed.hd.basic
-echo
 include ../preseed/preseed.ntp
-echo
 include ../preseed/preseed.repos-n-updates
-
-
-#cat defaults.cfg.template
-
