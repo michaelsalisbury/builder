5f0f455c85ebeb0d1126f98d5815b7e977b68ed3
root <root@DLPBMM1.cos.ucf.edu>
2013-02-08 10:44:55 -0500

    2013 Feb  8 [Fri] 10:44:55 AM :: root@DLPBMM1.cos.ucf.edu

--- kickstart/ubuntu/s.apt-cache/ks.sh
+++ kickstart/ubuntu/s.apt-cache/ks.sh
@@ -8,8 +8,13 @@ cat ../defaults.cfg.template
 echo
 cat packages.cfg
 echo
-
-
+#############################################################################
+cat ../post.chroot.get_VBoxGuestAdditions.cfg
+cat ../post.chroot.setup_root_auth_keys.cfg
+cat ../post.chroot.setup_ntpd.cfg
+cat ../post.chroot.interactive.cfg
+cat ../post.nochroot.setup_builder.cfg
+cat ../post.nochroot.interactive.cfg
 
 echo
 cat ../pre.interactive-n-download.cfg
