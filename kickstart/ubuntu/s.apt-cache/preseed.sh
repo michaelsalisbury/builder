4c0975d909eb8cc46fff073b3118091f9c7d24b8
root <root@DLPBMM1.cos.ucf.edu>
2013-02-08 10:26:27 -0500

    2013 Feb  8 [Fri] 10:26:27 AM :: root@DLPBMM1.cos.ucf.edu

---
+++ kickstart/ubuntu/s.apt-cache/preseed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Content-Type: text/plain"
+echo # DO NOT REMOVE THIS BLANK LINE
+#############################################################################
+cat local.cfg
+echo
+cat ../defaults.cfg.template
+echo
+
+
+#cat defaults.cfg.template
+
