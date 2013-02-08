856c11f7ab8ec6851f2757d019b9c35dc43ab520
root <root@DLPBMM1.cos.ucf.edu>
2013-02-08 10:47:51 -0500

    2013 Feb  8 [Fri] 10:47:51 AM :: root@DLPBMM1.cos.ucf.edu

--- kickstart/ubuntu/s.apt-cache/ks.sh
+++ kickstart/ubuntu/s.apt-cache/ks.sh
@@ -7,12 +7,11 @@ function include(){
 echo "Content-Type: text/plain"
 echo # DO NOT REMOVE THIS BLANK LINE
 #############################################################################
-cat local.cfg
-echo
-cat ../defaults.cfg.template
-echo
-cat packages.cfg
-echo
+include local.cfg
+include ../defaults.cfg.template
+include packages.cfg
+
+#############################################################################
 include ../post.chroot.get_VBoxGuestAdditions.cfg
 include ../post.chroot.setup_root_auth_keys.cfg
 include ../post.chroot.setup_ntpd.cfg
@@ -20,8 +19,5 @@ include ../post.chroot.interactive.cfg
 include ../post.nochroot.setup_builder.cfg
 include ../post.nochroot.interactive.cfg
 
-echo
+#############################################################################
 cat ../pre.interactive-n-download.cfg
-
-#cat defaults.cfg.template
-
