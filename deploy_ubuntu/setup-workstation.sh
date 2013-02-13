101152fc4279870912cd4be13756a9c1857225a4
root <root@DLPBMM1.cos.ucf.edu>
2013-02-13 15:54:58 -0500

    2013 Feb 13 [Wed] 03:54:58 PM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -690,33 +690,6 @@ function setup_Mistake(){
 	waitForNetwork || return 1
 	# Auto-responce
         echo acroread-common acroread-common/default-viewer select true | debconf-set-select
-	# Add Adobe Repo
-	if [ ! -f "/etc/apt/sources.list.d/canonical_Adobe.list" ]; do
-		for repo in								\
-		"deb http://archive.canonical.com/ubuntu precise partner"		\
-		"deb-src http://archive.canonical.com/ubuntu precise partner"		\
-		"deb http://archive.canonical.com/ubuntu `lsb_release -sc` partner"	\
-		"deb-src http://archive.canonical.com/ubuntu `lsb_release -sc` partner"
-		do echo ${repo} >> "/etc/apt/sources.list.d/canonical_Adobe.list"; done
-		waitAptgetUpdate
-		apt-get --quiet update
-	done
-#}
-#function setup_crap(){
-	# Add Medibuntu repo for free and non-free packages like acroread
-	if [ ! -f "/etc/apt/sources.list.d/medibuntu.list" ]; do
-		wget -O "/etc/apt/sources.list.d/medibuntu.list" "http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list"
-        	waitAptgetUpdate
-		apt-get --quiet update
-		apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring
-		apt-get --quiet update
-	done
-	# Add Oracle Java repo
-	if [ ! -f "/etc/apt/sources.list.d/webupd8team-java-quantal.list" ]; do
-		add-apt-repository -y ppa:webupd8team/java
-        	waitAptgetUpdate
-		apt-get --quiet update
-	done
 	# Install Acrobat Reader
         waitAptgetInstall
         apt-get ${aptopt} install acroread
