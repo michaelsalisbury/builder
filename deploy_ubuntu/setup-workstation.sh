56b2d397b77dc51fa6f18bd5bf448a9c92df6de2
root <root@DLPBMM1.cos.ucf.edu>
2013-02-13 15:45:46 -0500

    2013 Feb 13 [Wed] 03:45:46 PM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -693,16 +693,14 @@ function setup_adobe(){
         echo acroread-common acroread-common/default-viewer select true | debconf-set-select
 	# Add Adobe Repo
 	if [ ! -f "/etc/apt/sources.list.d/canonical_Adobe.list" ]; do
-
-	while read repo; do echo ${repo} >> "/etc/apt/sources.list.d/canonical_Adobe.list"
-	done << REPO-LIST
-		deb http://archive.canonical.com/ubuntu precise partner
-		deb-src http://archive.canonical.com/ubuntu precise partner
-		deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner
-		deb-src http://archive.canonical.com/ubuntu $(lsb_release -sc) partner
-REPO-LIST
-waitAptgetUpdate
-apt-get --quiet update
+		for repo in								\
+		"deb http://archive.canonical.com/ubuntu precise partner"		\
+		"deb-src http://archive.canonical.com/ubuntu precise partner"		\
+		"deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"	\
+		"deb-src http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+		do echo ${repo} >> "/etc/apt/sources.list.d/canonical_Adobe.list"; done
+		waitAptgetUpdate
+		apt-get --quiet update
 	done
 }
 finction setup_crap(){
