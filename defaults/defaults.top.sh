22a7e472dd9e856b655014711fbbb217bbe0beda
root <root@DLPBMM1.cos.ucf.edu>
2013-02-13 11:34:49 -0500

    2013 Feb 13 [Wed] 11:34:49 AM :: root@DLPBMM1.cos.ucf.edu

--- defaults/defaults.top.sh
+++ defaults/defaults.top.sh
@@ -11,68 +11,34 @@ done < <(ls -1              "${scriptPath}"/functions.*.sh 2> /dev/null
 
 function setup_skel_Structure(){
 	desc Build skel directory structure
-	mkdir -p  /etc/skel/.config/terminator
-	chmod 700 /etc/skel/.config
-	chmod 700 /etc/skel/.config/terminator
+	touch     /etc/skel/.toprc
+	chmod 700 /etc/skel/.toprc
 }
 function setup_make_Config(){
 	desc Setting up default config
-	cat << END-OF-CONFIG > /etc/skel/.config/terminator/config
-[global_config]
-[keybindings]
-[profiles]
-  [[default]]
-    copy_on_selection = True
-    use_system_font = False
-    font = Monospace 13
-    scrollbar_position = hidden
-    scrollback_infinite = True
-[layouts]
-  [[default]]
-    [[[child0]]]
-      position = 74:29
-      type = Window
-      order = 0
-      parent = ""
-      size = 1846, 1051
-    [[[child1]]]
-      position = 923
-      type = HPaned
-      order = 0
-      parent = child0
-    [[[child3]]]
-      position = 525
-      type = VPaned
-      order = 1
-      parent = child1
-    [[[terminal2]]]
-      profile = hp-dv6tqe
-      type = Terminal
-      order = 0
-      parent = child1
-    [[[terminal5]]]
-      profile = hp-dv6tqe
-      type = Terminal
-      order = 1
-      parent = child3
-      command = tail -f /var/log/messages /var/log/httpd/access_log
-    [[[terminal4]]]
-      profile = hp-dv6tqe
-      type = Terminal
-      order = 0
-      parent = child3
-[plugins]
+	cat << END-OF-CONFIG > /etc/skel/.toprc
+RCfile for "top with windows"           # shameless braggin'
+Id:a, Mode_altscr=0, Mode_irixps=1, Delay_time=3.000, Curwin=0
+Def     fieldscur=AEHIOQTWKNMbcdfgjplrsuvyzX
+        winflags=32569, sortindx=10, maxtasks=0
+        summclr=1, msgsclr=1, headclr=3, taskclr=1
+Job     fieldscur=ABcefgjlrstuvyzMKNHIWOPQDX
+        winflags=62777, sortindx=0, maxtasks=0
+        summclr=6, msgsclr=6, headclr=7, taskclr=6
+Mem     fieldscur=ANOPQRSTUVbcdefgjlmyzWHIKX
+        winflags=62777, sortindx=13, maxtasks=0
+        summclr=5, msgsclr=5, headclr=4, taskclr=5
+Usr     fieldscur=ABDECGfhijlopqrstuvyzMKNWX
+        winflags=62777, sortindx=4, maxtasks=0
+        summclr=3, msgsclr=3, headclr=2, taskclr=3
 END-OF-CONFIG
-	chmod 700 /etc/skel/.config/terminator/config
 }
 function setup_distribute_Config(){
 	desc setting up default config \for existing users
 	get_user_details all | while read user uid gid home; do
 		su -m ${user} < <(cat << END-OF-CMDS
-			mkdir -p  "${home}/.config/terminator"
-			chmod 700 "${home}/.config/"
-			chmod 700 "${home}/.config/terminator"
-			cp "/etc/skel/.config/terminator/config" "${home}/.config/terminator/."
+			cp "/etc/skel/.toprc" "${home}/."
+			chmod 700 "${home}/.toprc"
 END-OF-CMDS
 )
 	done
