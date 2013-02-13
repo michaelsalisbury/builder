5b3caefaf4b27551d5d56bbd40f680fb9083fc45
root <root@DLPBMM1.cos.ucf.edu>
2013-02-13 15:56:58 -0500

    2013 Feb 13 [Wed] 03:56:58 PM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -702,8 +702,8 @@ function setup_Mistake(){
 		apt-get --quiet update
 	#done
 	# Add Medibuntu repo for free and non-free packages like acroread
-	if [ -f "/etc/apt/sources.list.d/medibuntu.list" ]
-	do
+	if true; do 
+	#[ -f "/etc/apt/sources.list.d/medibuntu.list" ]
 		wget -O "/etc/apt/sources.list.d/medibuntu.list" "http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list"
         	waitAptgetUpdate
 		apt-get --quiet update
