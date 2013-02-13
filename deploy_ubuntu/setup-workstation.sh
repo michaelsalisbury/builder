be556a3f87a5bb68a2c7101cc8ca42f6958b4e42
root <root@DLPBMM1.cos.ucf.edu>
2013-02-13 13:38:19 -0500

    2013 Feb 13 [Wed] 01:38:19 PM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -669,7 +669,7 @@ function setup_Crossover(){
 	waitForNetwork || return 1
 	# dependencies
         waitAptgetInstall
-	apt-get ${aptopt} install gdebi
+	apt-get ${aptopt} install gdebi libc6-i386
 	# setup working dir
 	mkdir /root/codeweavers_crossover
 	cd    /root/codeweavers_crossover
