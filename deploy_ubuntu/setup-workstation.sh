695d6da940aed90403aac3cb502fd1a9b34a8964
root <root@DLPBMM1.cos.ucf.edu>
2013-02-15 12:46:27 -0500

    2013 Feb 15 [Fri] 12:46:27 PM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -161,9 +161,10 @@ END-OF-MKDIR
 
 function setup_Prep_Hostname(){
 	desc \set hostname to vendor serial: Dell
+	opts='-o ppid --no-heading'
 	echo "  PID" = `ps -o pid,ppid,cmd --no-heading -p $$`
-	#echo " PPID" = `ps -o pid,ppid,cmd --no-heading -p $(ps $opts -p $$)`
-	#echo "PPPID" = `ps -o pid,ppid,cmd --no-heading -p $(ps $opts -p $(ps $opts -p $$))`
+	echo " PPID" = `ps -o pid,ppid,cmd --no-heading -p $(ps $opts -p $$)`
+	echo "PPPID" = `ps -o pid,ppid,cmd --no-heading -p $(ps $opts -p $(ps $opts -p $$))`
 	${scriptPath}/../defaults/defaults.terminator.sh -i layout
 
 
