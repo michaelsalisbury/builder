2efc6f7462148182911a235de335c4da6b2076f5
root <root@DLPBMM1.cos.ucf.edu>
2013-02-15 13:08:15 -0500

    2013 Feb 15 [Fri] 01:08:15 PM :: root@DLPBMM1.cos.ucf.edu

--- defaults/defaults.terminator.sh
+++ defaults/defaults.terminator.sh
@@ -111,10 +111,10 @@ function setup_runonce_layout(){
 
 	# get calling funtion log path
 	local ppid=$(ps -o ppid --no-heading -p $$)
-	local  cmd=`ps -o  cmd --no-heading -p ${ppid} | sed "s|/bin/bash||;s|${buildScriptFQFN}||;s|.sh.*||"`
+	ps -o  cmd --no-heading -p ${ppid} | sed "s|/bin/bash||;s|${buildScriptFQFN}||;s|.sh.*||"
 	#cmd=${cmd//\/bin\/bash/}
 	#cmd=${cmd//${buildScriptFQFN}/}
-	basename $cmd
+	#echo $cmd
 	basename `ps -o cmd -p $(ps -o ppid --no-heading -p $$) | awk '{print $3}'` .sh	
 
 }
