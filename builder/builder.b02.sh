05d621c68db766a6ad07298f58e596c092465660
root <root@DLPBMM1.cos.ucf.edu>
2013-02-25 18:14:23 -0500

    2013 Feb 25 [Mon] 06:14:23 PM :: root@DLPBMM1.cos.ucf.edu

--- builder/builder.b02.sh
+++ builder/builder.b02.sh
@@ -78,8 +78,8 @@ function switches(){
 					wrap; disp_functions; echo;;
 			i)		step=`find_function $OPTARG`
 					eval_function $step
-					
-					ls -l `log_get_name $step`
+					log_is_empty $step && disp_functions	
+					#ls -l `log_get_name $step`
 					;;
 			#i)		find_function $OPTARG		\
 			#		&& eval_function `find_function $OPTARG` \
