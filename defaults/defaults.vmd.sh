50c30c5e8621e2abacf8b788648305514f40790b
root <root@DLPBMM1.cos.ucf.edu>
2013-03-27 15:02:30 -0400

    2013 Mar 27 [Wed] 03:02:30 PM :: root@DLPBMM1.cos.ucf.edu

---
+++ defaults/defaults.vmd.sh
@@ -0,0 +1,66 @@
+#!/bin/builder.sh
+skip=( false false false false )
+step=1
+prefix="setup"
+source=http://10.173.119.78/scripts/system-setup/$scriptName
+
+while read import; do
+	. "${import}"
+done < <(ls -1              "${scriptPath}"/functions.*.sh 2> /dev/null
+	 ls -1 "${scriptPath}"/../functions/functions.*.sh 2> /dev/null)
+
+
+function setup_make_Config(){
+	desc Setting up default config
+	local scriptBase=$(basename "${scriptName}" .sh)
+	# import UCF certificate
+	local caPath='/usr/share/ca-certificates/ucf.edu'
+	local caFile='UCF-WPA2_comodo_incommonca.crt'
+	mkdir -p "${caPath}"
+        cp "${scriptPath}/${scriptBase}.crt" "${caPath}/${caFile}"
+
+	# intigrate UCF certificate
+	echo $(basename "${caPath}")/${caFile} >> /etc/ca-certificates.conf
+	/usr/sbin/update-ca-certificates --fresh
+
+	# test for wireless adapter
+	if ! nm-tool | egrep -q "Type:[[:space:]]*802.11"; then
+		echo ERROR\! No Wireless Adapter Found.
+		return 1
+	fi
+	
+	# get local wifi apater mac address
+	local MAC=$(nm-tool |\
+		awk '/Type:[[:space:]]*802.11/,/HW Address/{if($0~"HW Address")print $3}')
+        	
+	# setup connection profile
+	cat << END-OF-SYSTEM-CONNECTION > /etc/NetworkManager/system-connections/UCF_WPA2
+[ipv6]
+method=ignore
+
+[connection]
+id=UCF_WPA2
+uuid=$(uuidgen)
+type=802-11-wireless
+timestamp=$(date "+%s")
+
+[802-11-wireless-security]
+key-mgmt=wpa-eap
+
+[802-11-wireless]
+ssid=UCF_WPA2
+mode=infrastructure
+mac-address=${MAC}
+security=802-11-wireless-security
+
+[802-1x]
+eap=peap;
+identity=
+ca-cert=${caPath}
+phase2-auth=mschapv2
+password=
+
+[ipv4]
+method=auto
+END-OF-SYSTEM-CONNECTION
+}
