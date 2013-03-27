f2bdf73f18ac9323b5b9caf3bac0d98e7ca467eb
root <root@DLPBMM1.cos.ucf.edu>
2013-03-27 10:48:58 -0400

    2013 Mar 27 [Wed] 10:48:58 AM :: root@DLPBMM1.cos.ucf.edu

--- defaults/defaults.UCF_WPA2-ucf.edu.sh
+++ defaults/defaults.UCF_WPA2-ucf.edu.sh
@@ -13,10 +13,42 @@ done < <(ls -1              "${scriptPath}"/functions.*.sh 2> /dev/null
 function setup_make_Config(){
 	desc Setting up default config
 	local scriptBase=$(basename "${scriptName}" .sh)
+	# get UCF certificate
 	local caPath='/usr/share/ca-certificates/ucf.edu/UCF-WPA2_comodo_incommonca.crt'
         cp "${scriptPath}/${scriptBase}.crt" "${caPath}"
+	# get local wifi apater mac address
+	local MAC=$(nm-tool |\
+		awk '/Type:[[:space:]]*802.11/,/HW Address/{if($0~"HW Address")print $3}')
         	
+	# setup connection profile
 	cat << END-OF-SYSTEM-CONNECTION > /etc/NetworkManager/system-connections/UCF_WPA2
+[ipv6]
+method=ignore
+
+[connection]
+id=UCF_WPA2
+uuid=$(uuidgen)
+type=802-11-wireless
+timestamp=$(date "+%s")
+
+[802-11-wireless-security]
+key-mgmt=wpa-eap
+
+[802-11-wireless]
+ssid=UCF_WPA2
+mode=infrastructure
+mac-address=${MAC}
+security=802-11-wireless-security
+
+[802-1x]
+eap=peap;
+identity=
+ca-cert=${caPath}
+phase2-auth=mschapv2
+password=
+
+[ipv4]
+method=auto
 
 
 
