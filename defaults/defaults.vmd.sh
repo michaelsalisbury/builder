7f270dca09a0d4d0249d35ffd1cc78822eedafc7
root <root@DLPBMM1.cos.ucf.edu>
2013-03-27 16:01:38 -0400

    2013 Mar 27 [Wed] 04:01:38 PM :: root@DLPBMM1.cos.ucf.edu

--- defaults/defaults.vmd.sh
+++ defaults/defaults.vmd.sh
@@ -9,12 +9,13 @@ while read import; do
 done < <(ls -1              "${scriptPath}"/functions.*.sh 2> /dev/null
 	 ls -1 "${scriptPath}"/../functions/functions.*.sh 2> /dev/null)
 
-source_app=http://10.173/119.78/packages/Computation/vmd-1.9.1.bin.LINUXAMD64.opengl.tar.gz
+source_app=http://10.173.119.78/packages/Computation/vmd-1.9.1.bin.LINUXAMD64.opengl.tar.gz
 
 function setup_make_Config(){
 	desc Setting up default config
 	# install dependencies for OPENGL FLTK TK NETCDF TCL
-	apt-get install libfltk1.3 libfltk1.3-dev libfltk-*1.3 \
+	apt-get -y install \
+			libfltk1.3 libfltk1.3-dev libfltk-*1.3 \
 			tcl tcl-dev \
 			tk  tk-dev \
 			netcdf* libnetcdf*
@@ -22,11 +23,11 @@ function setup_make_Config(){
 	# get vmd
 	mkdir /opt/vmd
 	cd    /opt/vmd
-	local    source_app_file=$(basename "${source_app}")
-	rm -f "${source_app_file}"
-	wget  "${source_app}"
+	local       source_app_file=$(basename "${source_app}")
+	rm -f    "${source_app_file}"
+	wget     "${source_app}"
 	tar -zxf "${source_app_file}"
-	local    source_app_path=$(find ./* -maxdepth 1 -type d -cmin 1)
+	local       source_app_path=$(find ./* -maxdepth 1 -type d -cmin 1)
 
 	# setup vmd
 	cd "${source_app_path}"
