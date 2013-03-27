e678415df7a07a79c7c5799f9b2259f533f89988
root <root@DLPBMM1.cos.ucf.edu>
2013-03-27 10:09:04 -0400

    2013 Mar 27 [Wed] 10:09:04 AM :: root@DLPBMM1.cos.ucf.edu

--- defaults/defaults.UCF_WPA2-ucf.edu.sh
+++ defaults/defaults.UCF_WPA2-ucf.edu.sh
@@ -10,46 +10,21 @@ done < <(ls -1              "${scriptPath}"/functions.*.sh 2> /dev/null
 	 ls -1 "${scriptPath}"/../functions/functions.*.sh 2> /dev/null)
 
 
-function setup_make_Config()
+function setup_make_Config(){
 	desc Setting up default config
-	
-	cat << END-OF-
+	local scriptBase=$(basename "${scriptName}" .sh)
+	local caPath='/usr/share/ca-certificates/ucf.edu/UCF-WPA2_comodo_incommonca.crt'
+        cp "${scriptPath}/${scriptBase}.crt" "${caPath}"
+        	
+	cat << END-OF-SYSTEM-CONNECTION > /etc/NetworkManager/system-connections/UCF_WPA2
+
+
+
+END-OF-SYSTEM-CONNECTION
 
 
 
 
-	cat << END-OF-ALIASES > /etc/profile.d/cifs.sh
-alias    mount.d='\${HOME}/.scripts/mount.domain_cifs.sh'
-alias  mount.dom='\${HOME}/.scripts/mount.domain_cifs.sh'
-alias domain.mnt='\${HOME}/.scripts/mount.domain_cifs.sh'
-END-OF-ALIASES
-	for domain in $g_domains; do
-		cat << END-OF-CONF > /etc/skel/.cifs-${domain}-cred
-username=[NID]
-password=[***]
-domain=${domain}
-END-OF-CONF
-	done
-	cat << END-OF-SUDOERS > /etc/sudoers.d/cifs
-# cifs
-%cifs ALL=(root) NOPASSWD:/sbin/mount.cifs
-%cifs ALL=(root) NOPASSWD:/bin/umount -t cifs -v /home/*
-END-OF-SUDOERS
-	cat << END-OF-DESKTOP > /etc/xdg/autostart/mount.domain_cifs.desktop
-[Desktop Entry]
-Type=Application
-Exec=/bin/bash -c \\\${HOME}/.scripts/mount.domain_cifs.sh
-Hidden=false
-NoDisplay=true
-Terminal=true
-X-GNOME-Autostart-enabled=true
-Name[en_US]=CIFS Automount
-Name=CIFS Automount
-Comment[en_US]=
-Comment=
-END-OF-DESKTOP
-	# Modify /etc/adduser.conf to include new group openconnect
-	add_default_group cifs
 }
 function setup_distribute_Config(){
 	desc setting up default config \for existing users
