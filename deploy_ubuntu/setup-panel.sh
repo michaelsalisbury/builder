1fc61c642483a463cf721e474637ed7b0521c57b
root <root@DLPBMM1.cos.ucf.edu>
2013-04-26 10:12:03 -0400

    2013 Apr 26 [Fri] 10:12:03 AM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-panel.sh
+++ deploy_ubuntu/setup-panel.sh
@@ -3,6 +3,14 @@
 
 # CPUFreq
 
-	sudo add-apt-repository ppa:artfwo/ppa
-	sudo apt-get update
-	sudo apt-get install indicator-cpufreq
+	#sudo add-apt-repository ppa:artfwo/ppa
+	#sudo apt-get update
+	#sudo apt-get install indicator-cpufreq
+
+# System Load Indicator
+sudo add-apt-repository ppa:indicator-multiload/stable-daily
+sudo apt-get update
+sudo apt-get install indicator-multiload
+
+# Stack Exchange Applet Indicator
+
