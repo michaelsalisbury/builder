498c20b79c809e85b5553c7586a72bda11921b69
root <root@DLPBMM1.cos.ucf.edu>
2013-04-26 10:19:08 -0400

    2013 Apr 26 [Fri] 10:19:08 AM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-panel.sh
+++ deploy_ubuntu/setup-panel.sh
@@ -3,35 +3,46 @@
 
 # CPUFreq
 
-	#sudo add-apt-repository -y ppa:artfwo/ppa
-	#sudo apt-get update
-	#sudo apt-get -y install indicator-cpufreq
-	#sudo apt-get -yf install
+	#add-apt-repository -y ppa:artfwo/ppa
+	#apt-get update
+	#apt-get -y install indicator-cpufreq
+	#apt-get -yf install
 
 # System Load Indicator
-	sudo add-apt-repository -y ppa:indicator-multiload/stable-daily
-	sudo apt-get update
-	sudo apt-get -y install indicator-multiload
-	sudo apt-get -yf install
+	add-apt-repository -y ppa:indicator-multiload/stable-daily
+	apt-get update
+	apt-get -y install indicator-multiload
+	apt-get -yf install
 
 # Stack Exchange Applet Indicator
 	#apt-get install stackapplet
-	#sudo apt-get -yf install
+	#apt-get -yf install
 
 # Virtual Box Indicator
-	sudo add-apt-repository -y ppa:michael-astrapi/ppa
-	sudo apt-get update
-	sudo apt-get -y install indicator-virtualbox
-	sudo apt-get -yf install
+	add-apt-repository -y ppa:michael-astrapi/ppa
+	apt-get update
+	apt-get -y install indicator-virtualbox
+	apt-get -yf install
 
 # Keylick indicator (num-lock, tab-lock, scroll-lock)
-	sudo add-apt-repository -y ppa:tsbarnes/indicator-keylock
-	sudo apt-get update
-	sudo apt-get -y install indicator-keylock
-	sudo apt-get -yf install
+	add-apt-repository -y ppa:tsbarnes/indicator-keylock
+	apt-get update
+	apt-get -y install indicator-keylock
+	apt-get -yf install
 
 # Radio Tray indicator
-	sudo add-apt-repository -y ppa:tsbarnes/indicator-keylock
-	sudo apt-get update
-	sudo apt-get -y install indicator-keylock
-	sudo apt-get -yf install
+	add-apt-repository -y ppa:tsbarnes/indicator-keylock
+	apt-get update
+	apt-get -y install indicator-keylock
+	apt-get -yf install
+
+# LAPTOP - Touchpad Indicator
+	add-apt-repository -y ppa:atareao/atareao
+	apt-get update
+	apt-get -y install touchpad-indicator
+	apt-get -yf install
+
+# LAPTOP - Battery Indicator
+	add-apt-repository -y ppa:iaz/battery-status
+	apt-get update
+	apt-get install battery-status
