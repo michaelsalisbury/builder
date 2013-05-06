49e39b0b309281d9156c76f96e96b103f1775b14
root <root@DLPBMM1.cos.ucf.edu>
2013-05-06 11:29:31 -0400

    2013 May  6 [Mon] 11:29:31 AM :: root@DLPBMM1.cos.ucf.edu

---
+++ functions/functions.ssh.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+source=http://10.173.119.78/scripts/system-setup/$scriptName
+LOG="~/.logs/ssh"
+DOM='ucf.edu'
+
+function main(){
+
+	SSH_VERIFY_PASSWORD $(whoami) 10.173.161.50 'COSTech2010!'
+	SSH_VERIFY_PASSWORD $(whoami) 10.173.161.50 'COSTech2010\!'
+	SSH_VERIFY_PASSWORD $(whoami) 10.173.161.50 'COSTblaech2010\!'
+	return 0
+
+	PUSH_KEYS
+	return 0
+	
+	GET_HOSTNAME_SIMPLE 10.173.158.26 ${DOM}
+	GET_HOSTNAME_DOM    10.173.158.26 ${DOM}
+
+	SSH_COPY_ID $(whoami) 10.173.161.254 'COSTech2010\!'
+	SSH_COPY_ID $(whoami) 10.173.161.50  'COSTech2010\!'
+	SSH_COPY_ID $(whoami) 10.173.161.50  'COSTech2010\!' ~/.ssh/id_rsa.2945star
+
+	SSH_COPY_ID root      10.173.161.50  '1qaz@WSX'
+	SSH_COPY_ID root      10.173.161.50  '1qaz@WSX'      ~/.ssh/id_rsa.2945star 
+
+
+	#IP_IS_UP ${IP}	&& echo "${IP}"
+	#GET_HOSTNAME_SIMPLE ${IP} ${DOM}
+	#GET_HOSTNAME_DOM    ${IP} ${DOM}
+	#GET_HOST_ENTRY      ${IP} ${DOM}
+}
+function PUSH_KEYS(){
+	local HOSTFILE=${1:- /etc/hosts}
+	[ ! -f "${HOSTFILE}" ] && local HOSTFILE=/etc/hosts
+	while read IP; do
+		GET_HOST_ENTRY ${IP} ${DOM}
+	done < <(cat /etc/hosts | awk '/^[0-9].*/{print $1}')
+
+	for USERNAME in root $(whoami); do
+		for KEY in ~/.ssh/id_rsa ~/.ssh/id_rsa.2945star; do
+			echo $USERNAME $KEY		
+
+		done
+	done
+}
+function SSH_COPY_ID(){
+	local USERNAME=$1
+	local IP=$2
+	local PASSWORD=$3
+	local KEY=${4:- "~/.ssh/id_rsa"}
+	local KEY=${4:+"${4%.pub}.pub"}
+	# verify that KEY file exists
+	if [ ! -f "${KEY}" ]; then
+		echo key \"${KEY}\" missing\!\! 1>&2
+		echo ERROR_SSH_COPY_ID_FOR_${USERNAME}_TO_${IP}${KEY:+_VIA_KEY_${KEY}}
+		return 1
+	# verify that host is up via echo responce
+	elif ! IP_IS_UP ${IP}; then
+		echo "${IP}_DOWN"
+		return 1
+	# verify that host needs the ssh key in the first place
+	elif ! HOST_NEEDS_SSHKEY ${USERNAME} ${IP} ${KEY:+"${KEY}"}; then
+		echo ${USERNAME}_HAS_ACCESS_TO_${IP}${KEY:+_VIA_KEY_${KEY}}
+	# if the host grants access via the default key run ssh-copy-id without expect
+	elif ! HOST_NEEDS_SSHKEY ${USERNAME} ${IP}; then
+		ssh-copy-id ${KEY:+-i ${KEY}} ${USERNAME}@${IP} &> /dev/null\
+			&& echo ${USERNAME}_GRANTED_ACCESS_TO_${IP}${KEY:+_VIA_KEY_${KEY}}\
+			|| echo ERROR_SSH_COPY_ID_FOR_${USERNAME}_TO_${IP}${KEY:+_VIA_KEY_${KEY}}
+	# verify ssh username and password before running expect script
+	elif ! SSH_VERIFY_PASSWORD ${USERNAME} ${IP} ${PASSWORD}; then
+		echo ERROR_SSH_COPY_ID_FOR_${USERNAME}_TO_${IP}${KEY:+_VIA_KEY_${KEY}}
+		return 1
+	# copy ssh key via expect script
+	else
+		expect <(GET_EXPECT_SSH_COPY_ID ${USERNAME} ${IP} ${PASSWORD} ${KEY}) &> /dev/null\
+			&& echo ${USERNAME}_GRANTED_ACCESS_TO_${IP}${KEY:+_VIA_KEY_${KEY}}\
+			|| echo ERROR_SSH_COPY_ID_FOR_${USERNAME}_TO_${IP}${KEY:+_VIA_KEY_${KEY}}
+	fi
+} 
+function SSH_VERIFY_PASSWORD(){
+	local USERNAME=$1
+	local IP=$2
+	local PASSWORD=$3
+	! IP_IS_UP ${IP} && { echo IP \"${IP}\" is inaccesable\! 1>&2; return 1;}
+	expect <(GET_EXPECT_SSH_VERIFY_PASSWORD ${USERNAME} ${IP} ${PASSWORD}) &> /dev/null\
+		&& return 0\
+		|| { echo username[${USERNAME}] or password[${PASSWORD}] is incorrect\! 1>&2; return 1;}
+}
+function HOST_NEEDS_SSHKEY(){
+	local USERNAME=$1
+	local IP=$2
+	local KEY=${3%.pub}
+	[ -n "${KEY}" ] && [ ! -f "${KEY}" ] && { echo key \"${KEY}\" missing\!\! 1>&2; return 1;} 
+	ssh ${KEY:+-i "${KEY}"} -l ${USERNAME} -o passwordauthentication=no ${IP} exit &> /dev/null\
+		&& return 1 \
+		|| return 0
+}
+function GET_HOST_ENTRY(){
+	local IP=$1
+	local DOM=$2
+	if IP_IS_UP ${IP}; then
+		local SIMPLE=$(GET_HOSTNAME_SIMPLE "${IP}" "${DOM}")
+		local    DOM=$(GET_HOSTNAME_DOM    "${IP}" "${DOM}")
+		echo "${IP}" "${DOM}" "${SIMPLE}"
+	else
+		echo -n ""
+	fi
+}
+function IP_IS_UP(){
+	ping -W 1 -c 1 $1 &> /dev/null	\
+		&& return 0		\
+		|| return 1
+}
+function GET_HOSTNAME_SIMPLE(){
+	local IP=$1
+	local DOM=$2
+	local NAME=$(cat /etc/hosts		|\
+		sed 's/[[:space:]]*#.*$//'	|\
+		awk	-v IP="${IP}"		\
+			-v DOM="${DOM}"		\
+			'$0~"^"IP{for(i=2;i<=NF;i++)if(tolower($i)!~tolower(DOM))printf $i" "}')
+	[ -n "${NAME}" ]\
+		&& echo "${NAME}" \
+		|| echo "-"
+}
+function GET_HOSTNAME_DOM(){
+	local IP=$1
+	local DOM=$2
+	local NAME=$(cat /etc/hosts		|\
+		sed 's/[[:space:]]*#.*$//'	|\
+		awk	-v IP="${IP}"		\
+			-v DOM="${DOM}"		\
+			'$0~"^"IP{for(i=1;i<=NF;i++)if(tolower($i)~tolower(DOM))printf $i" "}')
+	[ -n "${NAME}" ]\
+		&& echo "${NAME}" \
+		|| echo "-"
+}
+function GET_EXPECT_SSH_VERIFY_PASSWORD(){
+	local USERNAME=$1
+	local IP=$2
+	local PASSWORD=$3
+	cat << END-OF-EXPECT
+#!/usr/bin/expect -f
+set timeout -1
+spawn ssh -o NumberOfPasswordPrompts=1 -o PubkeyAuthentication=no ${USERNAME}@${IP} exit
+match_max 100000
+expect *
+expect  -exact "password: "
+send -- "${PASSWORD}\r"
+expect *
+send -- "exit\r"
+expect eof
+END-OF-EXPECT
+}
+function GET_EXPECT_SSH_COPY_ID(){
+	local USERNAME=$1
+	local IP=$2
+	local PASSWORD=$3
+	local KEY=${4:+"${4%.pub}.pub"}
+	cat << END-OF-EXPECT
+#!/usr/bin/expect -f
+set timeout -1
+spawn ssh-copy-id ${KEY:+-i ${KEY}} ${USERNAME}@${IP}
+match_max 100000
+expect *
+expect  -exact "password: "
+send -- "${PASSWORD}\r"
+expect eof
+END-OF-EXPECT
+}
+
+main "$@"
