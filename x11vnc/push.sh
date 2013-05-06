34927f54a4980b09b9140e2ae855ae5aea1ed48b
root <root@DLPBMM1.cos.ucf.edu>
2013-05-06 11:38:15 -0400

    2013 May  6 [Mon] 11:38:15 AM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/push.sh
+++ x11vnc/push.sh
@@ -6,6 +6,18 @@ packagename=$(basename "${foldername}")
 # create tgz
 tar -zcvf "${packagename}.tgz" *
 
+# folder list to copy package to
+while read path; do
+	echo $path
+
+
+done << PATH-LIST
+	/var/www/packages/Apps_Linux
+
+
+
+PATH-LIST
+
 
 
 
