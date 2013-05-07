253bd218def8d950f765495edca88bf6da0e492a
root <root@DLPBMM1.cos.ucf.edu>
2013-05-07 17:32:55 -0400

    2013 May  7 [Tue] 05:32:55 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/push.sh
+++ x11vnc/push.sh
@@ -35,6 +35,7 @@ while read IP HOST OTHER; do
 	#SSH_COPY_ID         ${USERNAME} ${IP} ${PASSWORD} "/home/localcosadmin/.ssh/id_rsa.2945star"
 	echo
 	echo 123
+	continue
 	
 done << HOST-LIST
 	10.171.252.38	dr-richardson-ch0	vnmrs500	BTMZRW1.cos.ucf.edu
