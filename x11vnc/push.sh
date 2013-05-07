e349abbd5dfa924f02a99ee444f90416fa67e3eb
root <root@DLPBMM1.cos.ucf.edu>
2013-05-07 16:43:02 -0400

    2013 May  7 [Tue] 04:43:02 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/push.sh
+++ x11vnc/push.sh
@@ -27,7 +27,7 @@ PATH-LIST
 # host list to rsync updates to
 while read IP HOST OTHER; do
 	echo -n $IP
-	SSH_VERIFY_PASSWORD ${USERNAME} ${IP} ${PASSWORD} && echo ::GOOD 
+	SSH_VERIFY_PASSWORD ${USERNAME} ${IP} ${PASSWORD} && echo ::GOOD[$HOST] 
 
 done << HOST-LIST
 	10.171.252.38	dr-richardson-ch0	vnmrs500	BTMZRW1.cos.ucf.edu
