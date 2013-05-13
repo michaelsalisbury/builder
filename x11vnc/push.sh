678cce2183f2804f000e74d47fe2ceed1f17fd46
root <root@DLPBMM1.cos.ucf.edu>
2013-05-13 18:40:44 -0400

    2013 May 13 [Mon] 06:40:44 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/push.sh
+++ x11vnc/push.sh
@@ -8,9 +8,18 @@ function includes(){
 	../functions/functions.*.sh
 }
 function global_variables(){
-	SSH_USERNAME=localcosadmin
+	SSH_USERNAME='localcosadmin'
 	SSH_PASSWORD='COSTech2010\!'
 	push_packagename=$(basename "${scriptPath}")
+}
+function push_host_list(){
+	cat << HOST-LIST
+		10.171.252.38	dr-richardson-ch0	vnmrs500	BTMZRW1.cos.ucf.edu
+		10.171.252.95	dr-richardson-ch1	mercury300	5T2TNC1.cos.ucf.edu
+		10.173.152.119	dr-richardson-ps	avance400	BTN0SW1.cos.ucf.edu
+		10.173.156.190	dr-jameshopper-nmr	Agilent-NMR	2UA20814VX.cos.ucf.edu
+		10.173.152.117	dr-bochen-nmr		SSNMR
+HOST-LIST
 }
 function push_main(){
 	desc main
@@ -32,21 +41,12 @@ function push_test(){
 	# host list to rsync updates to
 	while read IP HOST OTHER; do
 		echo -n $IP
-		SSH_VERIFY_PASSWORD ${USERNAME} ${IP} ${PASSWORD} && echo ::GOOD[$HOST]
-		SSH_COPY_ID         ${USERNAME} ${IP} ${PASSWORD} "/root/.ssh/id_rsa"
-		SSH_COPY_ID         ${USERNAME} ${IP} ${PASSWORD} "/home/localcosadmin/.ssh/id_rsa"
-		SSH_COPY_ID         ${USERNAME} ${IP} ${PASSWORD} "/home/localcosadmin/.ssh/id_rsa.2945star"
+		SSH_VERIFY_PASSWORD ${SSH_USERNAME} ${IP} ${SSH_PASSWORD} && echo ::GOOD[$HOST]
+		SSH_COPY_ID         ${SSH_USERNAME} ${IP} ${SSH_PASSWORD} "/root/.ssh/id_rsa"
+		SSH_COPY_ID         ${SSH_USERNAME} ${IP} ${SSH_PASSWORD} "/home/localcosadmin/.ssh/id_rsa"
+		SSH_COPY_ID         ${SSH_USERNAME} ${IP} ${SSH_PASSWORD} "/home/localcosadmin/.ssh/id_rsa.2945star"
 	
 	done < <(push_host_list)
 }
 
-function push_host_list(){
-	cat << HOST-LIST
-		10.171.252.38	dr-richardson-ch0	vnmrs500	BTMZRW1.cos.ucf.edu
-		10.171.252.95	dr-richardson-ch1	mercury300	5T2TNC1.cos.ucf.edu
-		10.173.152.119	dr-richardson-ps	avance400	BTN0SW1.cos.ucf.edu
-		10.173.156.190	dr-jameshopper-nmr	Agilent-NMR	2UA20814VX.cos.ucf.edu
-		10.173.152.117	dr-bochen-nmr		SSNMR
-HOST-LIST
-}
 
