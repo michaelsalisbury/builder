a2e2263fa2330ab0dd54a062a55d3c0a43ddf19d
root <root@DLPBMM1.cos.ucf.edu>
2013-05-15 09:06:46 -0400

    2013 May 15 [Wed] 09:06:46 AM :: root@DLPBMM1.cos.ucf.edu

--- deploy_ubuntu/setup-workstation.sh
+++ deploy_ubuntu/setup-workstation.sh
@@ -593,13 +593,15 @@ function setup_Live_Distro_Desktop_Session(){
 ###########################################################################################
 ###########################################################################################
 function setup_Configure_SSH(){
-	desc SSH: disable GSSAPIAuth \& disable UseDNS
+	desc SSH: disable GSSAPIAuth, UseDNS, ecdsa keys
 	# Disable GSSAPIAuthentication
         sed /etc/ssh/sshd_config -i \
-		-e '\|^[^#]*GSSAPIAuthentication[[:space:]]\+yes|s|^|#|'
+		-e '\|^[^#]*GSSAPIAuthentication[[:space:]]\+|s|^|#|'
+		-e '$a\GSSAPIAuthentication no'
 	# Disable DNS verification
         sed /etc/ssh/sshd_config -i \
-		-e '\|^[^#]*UseDNS|s|^|#|;$aUseDNS no'
+		-e '\|^[^#]*UseDNS|s|^|#|'\
+		-e '$a\UseDNS no'
 	# Disable HostKey /etc/ssh/ssh_host_ecdsa_key
         sed /etc/ssh/sshd_config -i \
 		-e '\|^[^#]*HostKey[[:space:]]\+/etc/ssh/ssh_host_ecdsa_key|s|^|#|'
