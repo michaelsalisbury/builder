c718d9c6eda5962691679bee87a0bd532cfd9dd3
root <root@DLPBMM1.cos.ucf.edu>
2013-05-22 14:42:16 -0400

    2013 May 22 [Wed] 02:42:16 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/install.redhat.sh
+++ x11vnc/install.redhat.sh
@@ -3,6 +3,9 @@
 # requires;
 #	x11vnc-0.9.13-8.el6.x86_64 from rmpforge
 #	xorg-x11-server-Xvfb-1.7.7-29.el6.x86_64.rpm for RHEL 6.1 from subscription
+yum install libXfont xorg-x11-xfs xorg-x11-xfs-utils xorg-x11-xinit xorg-x11-xdm
+yum install pixman pixman-devel libXfont
+
 
 # verify that x11vnc is installed
 
