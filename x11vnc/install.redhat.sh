c434009ea8643a0aad8b4b80ca50d99b4d5770b8
root <root@DLPBMM1.cos.ucf.edu>
2013-05-22 12:27:11 -0400

    2013 May 22 [Wed] 12:27:11 PM :: root@DLPBMM1.cos.ucf.edu

---
+++ x11vnc/install.redhat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# requires;
+#	x11vnc-0.9.13-8.el6.x86_64 from rmpforge
+#	xorg-x11-server-Xvfb-1.7.7-29.el6.x86_64.rpm for RHEL 6.1 from subscription
+
+# verify that x11vnc is installed
+
+
+# verify that xinetd is running
+
+
