7e0534ac774c408522700714e08a3e345b35b4fc
root <root@DLPBMM1.cos.ucf.edu>
2013-05-22 14:42:33 -0400

    2013 May 22 [Wed] 02:42:33 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc/install.redhat.sh
+++ x11vnc/install.redhat.sh
@@ -5,6 +5,7 @@
 #	xorg-x11-server-Xvfb-1.7.7-29.el6.x86_64.rpm for RHEL 6.1 from subscription
 yum install libXfont xorg-x11-xfs xorg-x11-xfs-utils xorg-x11-xinit xorg-x11-xdm
 yum install pixman pixman-devel libXfont
+yum install tigervnc-server
 
 
 # verify that x11vnc is installed
