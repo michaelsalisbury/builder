922c85013b35135353b91bc5f119939aeafd99cb
root <root@DLPBMM1.cos.ucf.edu>
2013-05-31 16:37:13 -0400

    2013 May 31 [Fri] 04:37:13 PM :: root@DLPBMM1.cos.ucf.edu

---
+++ x11vnc_solaris/test/install.redhat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# requires;
+#	x11vnc-0.9.13-8.el6.x86_64 from rmpforge
+#	xorg-x11-server-Xvfb-1.7.7-29.el6.x86_64.rpm for RHEL 6.1 from subscription
+yum install libXfont xorg-x11-xfs xorg-x11-xfs-utils xorg-x11-xinit xorg-x11-xdm
+yum install pixman pixman-devel libXfont
+yum install tigervnc-server
+
+
+# verify that x11vnc is installed
+
+
+# verify that xinetd is running
+
+
