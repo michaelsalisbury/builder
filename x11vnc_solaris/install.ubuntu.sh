26b4dc26f7c59eddd9011e37befb7e2e33530e70
root <root@DLPBMM1.cos.ucf.edu>
2013-06-02 19:32:25 -0400

    2013 Jun  2 [Sun] 07:32:25 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc_solaris/install.ubuntu.sh
+++ x11vnc_solaris/install.ubuntu.sh
@@ -8,3 +8,14 @@ function main(){
 
 
 }
+# GLOBAL vars; fully qualified script paths and names
+BASH_SRCFQFN=$(canonicalpath "${BASH_SOURCE}")
+BASH_SRCNAME=$(basename "${BASH_SRCFQFN}")
+BASH_SRCDIR=$(dirname "${BASH_SRCFQFN}")
+
+
+
+
+
+
+main "$@"
