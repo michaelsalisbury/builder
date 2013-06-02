b8cbfbdbd707711a35981613172d4fb30739e246
root <root@DLPBMM1.cos.ucf.edu>
2013-06-02 19:38:14 -0400

    2013 Jun  2 [Sun] 07:38:14 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc_solaris/install.ubuntu.sh
+++ x11vnc_solaris/install.ubuntu.sh
@@ -13,7 +13,10 @@ BASH_SRCFQFN=$(canonicalpath "${BASH_SOURCE}")
 BASH_SRCNAME=$(basename "${BASH_SRCFQFN}")
 BASH_SRCDIR=$(dirname "${BASH_SRCFQFN}")
 
+# Source git repo sudirectory
+http='https://raw.github.com/michaelsalisbury/builder/master/x11vnc_solaris'
 
+# 
 
 
 
