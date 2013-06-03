85df6ee16b58d287ed391caa92e177eae94584ff
root <root@DLPBMM1.cos.ucf.edu>
2013-06-03 13:57:54 -0400

    2013 Jun  3 [Mon] 01:57:54 PM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc_solaris/install.ubuntu.sh
+++ x11vnc_solaris/install.ubuntu.sh
@@ -70,9 +70,9 @@ function main(){
 
 	fi
 }
-function MOVE_TO_ETC{
+function MOVE_TO_ETC(){
 	# Dependant on GLOBAL var "NAME"
-	local latest=$(GET_LATEST)
+	local latest=`GET_LATEST`
 	local version=`echo "${latest}" | head -1 | xargs dirname`
 	local version_dir="/etc/${NAME}/${version}"
 	rm    -rf "${version_dir}"
