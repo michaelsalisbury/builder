8b2405e0640617bce3f93fedcd317442d9b2de0a
root <root@DLPBMM1.cos.ucf.edu>
2013-06-03 10:54:24 -0400

    2013 Jun  3 [Mon] 10:54:24 AM :: root@DLPBMM1.cos.ucf.edu

--- x11vnc_solaris/install.ubuntu.sh
+++ x11vnc_solaris/install.ubuntu.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
+function IS_IN_ETC(){
+	# Dependant on GLOBAL var "NAME"
+	local latest=`GET_LATEST`
+	local version=`echo "${latest}" | head -1 | xargs dirname`
+	local version_dir="/etc/${NAME}/${version}"
+	[ "${BASH_SRCDIR}" == "${version_dir}" ]
+}
+function MOVE_TO_ETC{
 
 
+
+
+}
 function main(){
 	local version=`echo "${latest}" | head -1 | xargs dirname`
 	local version_dir="/etc/${NAME}/${version}"
 	
 	# Verify that currect version is latest
-	diff <(echo "${latest}") "${BASH_SRCDIR}/LATEST.TXT" &>/dev/null
 	# If an update is need then re-download and re-run install
-	if (( $? != 0 )); then
-		rm -rf    "${version_dir}"
-		mkdir  -p "${version_dir}"
-		cd        "${version_dir}"
-		local part=""
-		for part in ${latest}; do
-			wget "${http}/${part}"
-		done
-		cat * | tar -zxvf -
-		./"${BASH_SRCNAME}"
+	if ! VERSION_IS_CURRENT; then
+		DOWNLOAD_UPDATE
 		return
 	fi
 
 	# Move install into etc if nessisary and re-run install
-	if [ "${BASH_SRCDIR}" != "${version_dir}" ]; then
+	if ! IS_IN_ETC; then
+		MOVE_TO_ETC
+		return
+	fi
 		rm    -rf "${version_dir}"
 		mkdir  -p "${version_dir}"
 		cd        "${version_dir}"
@@ -60,6 +65,38 @@ function main(){
 
 
 }
+function DOWNLOAD_UPDATE(){
+	# Dependant on GLOBAL var "NAME"
+	local latest=`GET_LATEST`
+	local version=`echo "${latest}" | head -1 | xargs dirname`
+	local version_dir="/etc/${NAME}/${version}"
+
+	rm -rf    "${version_dir}"
+	mkdir  -p "${version_dir}"
+	cd        "${version_dir}"
+	local part=""
+	for part in ${latest}; do
+		wget "${http}/${part}"
+	done
+	cat * | tar -zxvf -
+	./"${BASH_SRCNAME}"
+	exit
+}
+function VERSION_IS_CURRENT(){
+	local LATEST="${BASH_SRCDIR}/LATEST.TXT"
+	[ -x "${LATEST}" ] &&\
+	diff <(GET_LATEST) "${LATEST}" &>/dev/null
+}
+function GET_LATEST(){
+	# deplendant on GLOBAL var "http" and "latest"
+	if (( ${#latest} > 0 )); then
+		echo "${latest}"
+	else
+		latest=`wget -O - -o /dev/null "${http}/LATEST.TXT"`
+		echo "${latest}"
+	fi
+
+}
 function canonicalpath(){
 	if [ -d $1 ]; then
 		pushd $1 > /dev/null 2>&1
@@ -83,7 +120,7 @@ http='https://raw.github.com/michaelsalisbury/builder/master/x11vnc_solaris'
 # Project NAME
 NAME='x11vnc'
 
-# Get latest version details
+# Get details of the latest version
 latest=`wget -O - -o /dev/null "${http}/LATEST.TXT"`
 
 # Projects major scripts
