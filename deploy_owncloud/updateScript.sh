e2b91a6533c08a23eb7b7a050615d8fc584e541d
root <G88192RMXYK.cos.ucf.edu>
2013-07-29 15:49:45 -0400

    owncloud

--- deploy_owncloud/updateScript.sh
+++ deploy_owncloud/updateScript.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+wget    --recursive                             \
+        --timestamping                          \
+        --no-directories                        \
+        --level 1                               \
+        --cut-dirs 1                            \
+        --accept sh,sql,conf                    \
+        --directory-prefix /etc/owncloud        \
+        http://10.173.119.78/repos/github/michaelsalisbury_builder/builder/builder.sh
+
 wget	--recursive				\
 	--timestamping				\
 	--no-directories			\
