e3e168dd81bbc8c6a0e2603eb14b0de36a531d2d
root <G88192RMXYK.cos.ucf.edu>
2014-03-13 12:31:58 -0400

    DiskStation

--- ipxe/ubuntu.early_command.sh
+++ ipxe/ubuntu.early_command.sh
@@ -15,10 +15,11 @@ main(){
 	chroot                        /root /usr/bin/apt-file update
 
 	chroot_profile_d ${pass_env_to_live} aliases.sh #alias rdesktop.DLPBMM1-VB='rdesktop 10.173.119.93 -u mi164210 -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -x 0x81 -z -N -a 32 -g 1020x1227'
-	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.hd='rdesktop 10.173.119.78 -u mi164210 -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -r sound=local -x 0x81 -z -N -a 32 -g 1980x1020 &'
-	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.ld='rdesktop 10.173.119.78 -u mi164210 -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -r sound=local -x 0x81 -z -N -a 32 -g 1333x768 &'
-	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.xga='rdesktop 10.173.119.78 -u mi164210 -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -r sound=local -x 0x81 -z -N -a 32 -g 1024x768 &'
-	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.sxga='rdesktop 10.173.119.78 -u mi164210 -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -r sound=local -x 0x81 -z -N -a 32 -g 1280x1024 &'
+	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop='rdesktop -d cos -T Win7-x86_64 -k en-us -r clipboard:PRIMARYCLIPBOARD -r sound=local -x 0x81 -z -N -a 32'
+	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.hd='rdesktop   10.173.119.78 -u mi164210 -g 1980x1020 &'
+	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.ld='rdesktop   10.173.119.78 -u mi164210 -g 1333x768 &'
+	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.xga='rdesktop  10.173.119.78 -u mi164210 -g 1024x768 &'
+	chroot_profile_d ${pass_env_to_live} aliases.sh alias rdesktop.DLPBMM1-VB.sxga='rdesktop 10.173.119.78 -u mi164210 -g 1280x1024 &'
 
 	#pause_install_early /tmp/early-command-pause -n 30 .. /tmp/early-command-pause ..
 }
